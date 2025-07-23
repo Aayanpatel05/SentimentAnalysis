b4094ab7443a6f28a997764e89f20ad48261dcec
Aayanpatel05 <aayanpatel2005@gmail.com>
2025-07-22 20:06:22 -0400

    Update SentimentAnalysis.py

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,24 +1,25 @@
-from data_preprocessing import load_and_prepare_data
+from data_preprocessing import load_and_process_data
 from bert_embeddings import create_dataloaders
 from train_model import train_evaluate_tfidf_model, train_evaluate_bert_model
 
-# Path to your CSV file
-csv_path = 'tweets_sentiments_dataset.csv'
+df = load_and_process_data('tweets_sentiments_dataset.csv')
 
-# Step 1: Load and preprocess data
-clean_texts, labels = load_and_prepare_data(csv_path)
+clean_texts = df['stemmed_content'].tolist()
+raw_texts = df['clean_text'].tolist()
+labels = df['sentiment'].tolist()
 
-# Step 2: Train & evaluate TF-IDF + Logistic Regression model
+class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(labels), y=labels)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+class_weights = torch.tensor(class_weights, dtype=torch.float).to(device)
+
 print("\nTraining TF-IDF + Logistic Regression Model...")
 train_evaluate_tfidf_model(clean_texts, labels)
 
-# Step 3: Create BERT DataLoaders
 print("\nPreparing BERT Dataloaders...")
 train_texts, val_texts, train_labels, val_labels = train_test_split(
     clean_texts, labels, test_size=0.2, random_state=42)
 
 train_loader, val_loader = create_dataloaders(train_texts, val_texts, train_labels, val_labels)
 
-# Step 4: Fine-tune and evaluate BERT model
 print("\nTraining BERT Model...")
-train_evaluate_bert_model(train_loader, val_loader)
+train_evaluate_bert_model(train_loader, val_loader,class_weights=class_weights)
